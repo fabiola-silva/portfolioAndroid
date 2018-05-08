568ae43910e6eb6e5ae7fab614587ae4a6cd9d85
fabiola <fabiolasilvaoffice@gmail.com>
2018-05-08 15:47:14 -0400

    Initial commit

---
+++ app/src/main/java/com/example/yuri/portfolioacademico/VoluntarioActivity.java
@@ -0,0 +1,13 @@
+package com.example.yuri.portfolioacademico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoluntarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voluntario);
+    }
+}
